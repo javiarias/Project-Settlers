54dcb16e38b02677be5464a22435a63a2b2b468c
javiarias <javari02@ucm.es>
2018-11-17 19:07:15 +0100

    Editado código de teclado acorde al feedback del viernes
    -Ahora va por señales y funciones
    -También he cambiado el modo de pausa (va por un booleano)
    -Además, he cambiado la declaración de variables para que sean correctas (no se si tanto this es bueno, habrá que preguntar)

--- src/js/play_scene.js
+++ src/js/play_scene.js
@@ -1,7 +1,6 @@
 'use strict';
 var Classes = require("./classes.js");
 
-
 var PlayScene = {
   
   create: function () {
@@ -12,129 +11,143 @@ var PlayScene = {
     logo.anchor.setTo(0.5, 0.5);
 
     //misc variables
-    var timeScale = 0;
-    var currentTime = { "hour": 0, "buffer": 0};
-    var homelessArray = [];
-    var shiftStart = 8;
-    var shiftEnd = 20;
+    this.paused = true;
+    this.timeScale = 1;
+    this.currentTime = { "hour": 0, "buffer": 0};
+    this.homelessArray = [];
+    this.shiftStart = 8;
+    this.shiftEnd = 20;
 
     /////////GROUPS AND RESOURCES
-    var houseGroup;
-    var food = 0;
+    this.houseGroup;
+    this.food = 0;
 
-    var woodGroup;
-    var wood = 0;
+    this.woodGroup;
+    this.wood = 0;
 
-    var coalGroup;
-    var coal = 0;
+    this.coalGroup;
+    this.coal = 0;
 
-    var uraniumGroup;
-    var uranium = 0;
+    this.uraniumGroup;
+    this.uranium = 0;
 
-    var energyGroup;
-    var energy = 0;
+    this.energyGroup;
+    this.energy = 0;
 
     //etc...
     ////////////////////////////////////////////////////////////////////////////////////////////////
 
-    woodGroup = this.game.add.group();
-    coalGroup = this.game.add.group();
-    uraniumGroup = this.game.add.group();
+    this.houseGroup = this.game.add.group();
+    this.woodGroup = this.game.add.group();
+    this.coalGroup = this.game.add.group();
+    this.uraniumGroup = this.game.add.group();
+    this.energyGroup = this.game.add.group();
     //etc...
 
     //keyboard phaser
-    this.game.input.keyboard.onPressCallback = function (e) {
-      switch(e) {
-        case "1":
-          timeScale = 1;
-          break;
-        case "2":
-          timeScale = 2;
-          break;
-        case "3":
-          timeScale = 3;
-          break;
-        case " ":
-          timeScale = 0;
-          break;
-        case "k":
-          var aux = new Classes.Producer(this.game, 0, 0, "logo", 1);
-          aux.anchor.setTo(0.5, 0.5);
-          woodGroup.add(aux);
-          break;
+    var key_One = this.game.input.keyboard.addKey(Phaser.Keyboard.ONE);
+    key_One.onDown.add(setTimescale, this, 0, 1);
 
-      }
-      currentTime.buffer = 0;
+    var key_Two = this.game.input.keyboard.addKey(Phaser.Keyboard.TWO);
+    key_Two.onDown.add(setTimescale, this, 0, 2);
+
+    var key_Three = this.game.input.keyboard.addKey(Phaser.Keyboard.THREE);
+    key_Three.onDown.add(setTimescale, this, 0, 3);
+
+    var key_Space = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    key_Space.onDown.add(pauseTime, this);
+
+    var key_K = this.game.input.keyboard.addKey(Phaser.Keyboard.K);
+    key_K.onDown.add(buildWood, this);
+
+
+    
+
+    
+    function setTimescale(key, time){
+      this.timeScale = time;
+      this.currentTime.buffer = 0;
+    }
+
+    function pauseTime(){
+      this.paused = !this.paused;
+    }
+
+    function buildWood(){
+      var aux = new Classes.Producer(this.game, 0, 0, "logo", 1);
+      aux.anchor.setTo(0.5, 0.5);
+      this.woodGroup.add(aux);
     }
   },
 
   update: function () {
 
-    currentTime.buffer += timeScale; //buffer increment
+    if(!this.paused){
+      this.currentTime.buffer += this.timeScale; //buffer increment
 
-    if (currentTime.buffer >=3) { //if buffer > 3, update. AKA, speed 1 = every 3 loops, speed 2 = every 2 loops... etc.
+      if (this.currentTime.buffer >=3) { //if buffer > 3, update. AKA, speed 1 = every 3 loops, speed 2 = every 2 loops... etc.
+        
+        //update clock
+        this.currentTime.buffer = 0;
+
+        this.currentTime.hour = (this.currentTime.hour + 1) % 24; 
       
-      //update clock
-      currentTime.buffer = 0;
+        ////////////////////////////////////////
+        //update producers
+        if(this.currentTime.hour >= this.shiftStart && this.currentTime.hour<= this.shiftEnd){
 
-      currentTime.hour = (currentTime.hour + 1) % 24; 
-    
-      ////////////////////////////////////////
-      //update producers
-      if(currentTime.hour >= shiftStart && currentTime.hour<= shiftEnd){
+          this.woodGroup.forEach(function(prod){
+            this.wood += prod.amount;
+          });
 
-        woodGroup.forEach(function(prod){
-          wood += prod.amount;
-        });
+          this.coalGroup.forEach(function(prod){
+            this.coal += prod.amount;
+          });
 
-        coalGroup.forEach(function(prod){
-          coal += prod.amount;
-        });
+          this.uraniumGroup.forEach(function(prod){
+            this.uranium += prod.amount;
+          });
 
-        uraniumGroup.forEach(function(prod){
-          uranium += prod.amount;
-        });
+          //etc...
 
-        //etc...
+          //update consumers
+          this.energyGroup.forEach(function(prod){
+            this.energy += prod.amount;
 
-        //update consumers
-        energyGroup.forEach(function(prod){
-          energy += prod.amount;
+            switch(prod.consumes){
 
-          switch(prod.consumes){
+              case "uranium":
+              this.uranium -= prod.consumed;
+                break;
 
-            case "uranium":
-              uranium -= prod.consumed;
-              break;
+                //other cases for other consumers
+            }
+          });
 
-              //other cases for other consumers
-          }
-        });
+          this.houseGroup.forEach(function(prod){
+            this.food -= prod.countCitizens * 5;
+          });
 
-        houseGroup.forEach(function(prod){
-          food -= prod.countCitizens * 5;
-        });
+          this.homelessArray.forEach(function(){
+            this.food -= 5;
+          });
+        }
+        
+        ////////////////////////////////////////
+        //update citizens
 
-        homelessArray.forEach(function(){
-          food -= 5;
-        });
+        //----DEBUG----
+        console.log(this.wood);
+        //console.log(currentTime.hour); 
+        //console.log("DING");
       }
       
-      ////////////////////////////////////////
-      //update citizens
+      else {
+        //----DEBUG----
 
-      //----DEBUG----
-
-      //console.log(currentTime.hour); 
-      //console.log("DING");
+        //console.log("waiting..."); 
+      }
     }
-    
-    else {
-      //----DEBUG----
-
-      //console.log("waiting..."); 
-    }
-    
   }
 };
 
