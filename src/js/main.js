18d4e428710bade1bf0fd3fd630c7240d0684eab
javiarias <javari02@ucm.es>
2018-12-09 12:26:31 +0100

    Actualizado dist aún más veces

--- src/js/main.js
+++ src/js/main.js
@@ -28,46 +28,46 @@ var PreloaderScene = {
   preload: function () {
 
     // TODO: load here the assets for the game
-    this.game.load.image('logo', 'assets/images/HERO.png');
-    this.game.load.image('test', 'assets/images/phaser.png');
-    this.game.load.image("fade", "assets/images/fade.png");
+    this.game.load.image('logo', 'images/HERO.png');
+    this.game.load.image('test', 'images/phaser.png');
+    this.game.load.image("fade", "images/fade.png");
 
     //building sprites
-    this.game.load.image('Crops', 'assets/images/buildings/Crops.png');
-    this.game.load.image('Energy', 'assets/images/buildings/Energy.png');
-    this.game.load.image('Hospital', 'assets/images/buildings/Hospital.png');
-    this.game.load.image('House', 'assets/images/buildings/House.png');
-    this.game.load.image('Stone', 'assets/images/buildings/Stone.png');
-    this.game.load.image('Coal', 'assets/images/buildings/Coal.png');
-    this.game.load.image('Road', 'assets/images/buildings/road.png');
-    this.game.load.image('Road Down', 'assets/images/buildings/road down.png');
-    this.game.load.image('Road Left', 'assets/images/buildings/road left.png');
-    this.game.load.image('Road Right', 'assets/images/buildings/road right.png');
-    this.game.load.image('Road Up', 'assets/images/buildings/road up.png');
-    this.game.load.image('Uranium', 'assets/images/buildings/Uranium.png');
-    this.game.load.image('Water', 'assets/images/buildings/Water.png');
-    this.game.load.image('Wind', 'assets/images/buildings/Wind.png');
-    this.game.load.image('Wood', 'assets/images/buildings/Wood.png');
+    this.game.load.image('Crops', 'images/buildings/Crops.png');
+    this.game.load.image('Energy', 'images/buildings/Energy.png');
+    this.game.load.image('Hospital', 'images/buildings/Hospital.png');
+    this.game.load.image('House', 'images/buildings/House.png');
+    this.game.load.image('Stone', 'images/buildings/Stone.png');
+    this.game.load.image('Coal', 'images/buildings/Coal.png');
+    this.game.load.image('Road', 'images/buildings/road.png');
+    this.game.load.image('Road Down', 'images/buildings/road down.png');
+    this.game.load.image('Road Left', 'images/buildings/road left.png');
+    this.game.load.image('Road Right', 'images/buildings/road right.png');
+    this.game.load.image('Road Up', 'images/buildings/road up.png');
+    this.game.load.image('Uranium', 'images/buildings/Uranium.png');
+    this.game.load.image('Water', 'images/buildings/Water.png');
+    this.game.load.image('Wind', 'images/buildings/Wind.png');
+    this.game.load.image('Wood', 'images/buildings/Wood.png');
 
 
   
     //map 
-    this.game.load.tilemap('tilemap', 'assets/images/map/map.json', null, Phaser.Tilemap.TILED_JSON);
-    this.game.load.image('patronesTilemap', 'assets/images/map/Tileset.png');
+    this.game.load.tilemap('tilemap', 'images/map/map.json', null, Phaser.Tilemap.TILED_JSON);
+    this.game.load.image('patronesTilemap', 'images/map/Tileset.png');
 
     //sounds
-    this.game.load.audio('menuSound', ['assets/sounds/menu.mp3', 'assets/sounds/menu.ogg']);
-    this.game.load.audio('gameSound', ['assets/sounds/game.mp3', 'assets/sounds/game.ogg']);
+    this.game.load.audio/('menuSound', ['audio/menu.mp3', 'audio/menu.ogg']);
+    this.game.load.audio/('gameSound', ['audio/game.mp3', 'audio/game.ogg']);
 
     //menus
-    this.game.load.image('pauseBkg', 'assets/images/menu/pauseMenu.png');
-    this.game.load.image('optionsBkg', 'assets/images/menu/optionsMenu.png');
-    this.game.load.spritesheet('exitBttn', 'assets/images/menu/exit.png', 55, 48);
-    this.game.load.spritesheet('minBttn', 'assets/images/menu/minimize.png', 55, 48);
-    this.game.load.spritesheet('settBttn', 'assets/images/menu/settings.png', 55, 48);
-    this.game.load.spritesheet('plusBttn', 'assets/images/menu/plus.png', 55, 48);
-    this.game.load.spritesheet('minusBttn', 'assets/images/menu/minus.png', 55, 48);
-    this.game.load.spritesheet('backBttn', 'assets/images/menu/back.png', 55, 48);
+    this.game.load.image('pauseBkg', 'images/menu/pauseMenu.png');
+    this.game.load.image('optionsBkg', 'images/menu/optionsMenu.png');
+    this.game.load.spritesheet('exitBttn', 'images/menu/exit.png', 55, 48);
+    this.game.load.spritesheet('minBttn', 'images/menu/minimize.png', 55, 48);
+    this.game.load.spritesheet('settBttn', 'images/menu/settings.png', 55, 48);
+    this.game.load.spritesheet('plusBttn', 'images/menu/plus.png', 55, 48);
+    this.game.load.spritesheet('minusBttn', 'images/menu/minus.png', 55, 48);
+    this.game.load.spritesheet('backBttn', 'images/menu/back.png', 55, 48);
 
   },
 
@@ -84,7 +84,7 @@ var MainMenu = {
     this.button = this.game.add.button(this.game.camera.x + (this.game.width/2), this.game.camera.y + 2.5 * (this.game.height/4), 'logo', gameStart, this);
     this.button.anchor.setTo(0.5, 0.5);
 
-    var menuMusic = this.game.add.audio('menuSound');
+    var menuMusic = this.game.add.audio/('menuSound');
     menuMusic.play();
     menuMusic.loop = true;
 
