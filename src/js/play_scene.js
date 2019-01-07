3cf540c4dd321a96a7d7bac66a558845ff3eaf18
javiarias <javari02@ucm.es>
2019-01-07 16:16:02 +0100

    Arreglado error al restar comida/agua

--- src/js/play_scene.js
+++ src/js/play_scene.js
@@ -47,6 +47,9 @@ var PlayScene = {
       this.gameMusic.play();
       this.gameMusic.volume = this.volume / 100;
 
+    //////////////////////////////
+    //misc. variables
+
       this.paused = true;
       this.timeScale = 1;
       this.currentTime = { "hour": 0, "buffer": 0};
@@ -90,6 +93,9 @@ var PlayScene = {
       this.energyGain = 0;
       this.waterGain = 0;
       this.stoneGain = 0;
+
+      this.citizenConsume = 3;
+      this.homelessConsume = 5;
 
     //////////////////////////////
     //Groups
@@ -1901,22 +1907,25 @@ var PlayScene = {
             this.timeTxt.addColor("#000000", 0);
 
             this.houseGroup.forEach(function(prod){
-              if(this.food >= 5)
-                this.food -= 3;
-              if(this.water >= 5)
-                this.water -= 3;
               prod.tick(this.food, this.water, prod.hospitalNear, prod, this.homelessArray, this.houseGroup);
+
+              var count = prod.countCitizens();
+
+              if(this.food >= this.citizenConsume)
+                this.food -= this.citizenConsume * count;
+              if(this.water >= this.citizenConsume)
+                this.water -= this.citizenConsume * count;
 
               for(var i = prod.numberOfBirths; i > 0; i--)
                 var aux = new Classes.Citizen(this.homelessArray, this.unemployedArray);
             }, this);
 
             for (var i = this.homelessArray.length - 1; i >= 0; i--) {
-              if(this.food >= 5)
-                this.food -= 5;
-              if(this.water >= 5)
-                this.water -= 5;
               this.homelessArray[i].tick(this.food, this.water, false, null, this.homelessArray, this.houseGroup);
+              if(this.food >= this.homelessConsume)
+                this.food -= this.homelessConsume;
+              if(this.water >= this.homelessConsume)
+                this.water -= this.homelessConsume;
             }
          
             this.foodTxt.text = this.food;
@@ -1965,10 +1974,10 @@ var PlayScene = {
           this.homelessTxt.text = this.homelessArray.length;
           this.unemployedTxt.text = this.unemployedArray.length;
           this.citizensTxt.text = aux;
-
+ 
           this.foodGain = 0;
           this.cropGroup.forEach(function(prod){this.foodGain += prod.amount * (this.shiftEnd - this.shiftStart)}, this);
-          this.foodGain -= (this.homelessArray.length * 5 + (aux - this.homelessArray.length ) * 3);
+          this.foodGain -= (this.homelessArray.length * this.homelessConsume + (aux - this.homelessArray.length ) * this.citizenConsume);
           var auxSymbol = "";
           var auxColor = "#FF0000";
           if(this.foodGain >= 0){
@@ -2004,7 +2013,7 @@ var PlayScene = {
 
           this.waterGain = 0;
           this.waterGroup.forEach(function(prod){this.waterGain += prod.amount * (this.shiftEnd - this.shiftStart)}, this);
-          this.waterGain -= (aux * 5);
+          this.waterGain -= (this.homelessArray.length * this.homelessConsume + (aux - this.homelessArray.length ) * this.citizenConsume);
           auxSymbol = "";
           auxColor = "#FF0000";
           if(this.waterGain >= 0){
